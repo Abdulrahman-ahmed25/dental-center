0cf5c07b8782b073220f4024a77eef5699b8fa34
Abdulrahman Ahmed <abdulrahman.ahmed2544@gmail.com>
2021-02-26 07:49:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// $(function () {
+//  'use strict';
+// //  Adjust height of slider
+// var windowHeight = $(window).height(),
+//       // upperHeight = $('.upper-bar').innerHeight(),
+//       navHeight   = $('.navbar').innerHeight();
+// $('.slider, .carousel-item').height(windowHeight - (navHeight));
+// //featured work shuffle
+// $('.featuredWork ul li').on('click', function () {
+//       $(this).addClass('active').siblings().removeClass('active');
+//       if($(this).data('class') == 'all'){
+//             $('.shuffle-imgs .image').css('opacity' , 1)
+//       }
+//       else{
+//             $('.shuffle-imgs .image').css('opacity' , '.08')
+//             $($(this).data('class')).parent().css('opacity' , 1);
+            
+//       }
+// });
+// });
